441ae4a8871b27cf3bbaa7a5d38b2a1a494c1b37
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-03-22 16:41:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,4 +20,30 @@ Webcam.set
 
    
    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelloaded);
-   
+
+   function modelloaded()
+{
+    console.log('model loaded');
+}
+
+function check()
+{
+    img = document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if( error )
+    {
+        console.log("error");
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        
+
+    }
+}
